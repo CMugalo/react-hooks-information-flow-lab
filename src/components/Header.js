1e015c95fc44a4e98c2a018605d5052a08058ada
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-05-24 06:44:32 +0300

    used callbacks to update state

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header({onDarkModeClick, mode}) {
+  return (
+    <div>
+      {" "}
+      <header>
+        <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          {{mode} ? "Dark" : "Light"} Mode
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
